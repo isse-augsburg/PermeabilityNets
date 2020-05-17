4d69e9d4feeab0f5a0aef9fb0056959cae1e1629
may <johannes.may@udo.edu>
2020-05-17 15:50:42 +0200

    Linter ❤

--- Utils/migrate_absolute_splits.py
+++ Utils/migrate_absolute_splits.py
@@ -4,14 +4,16 @@ import sys
 import pickle
 import os
 
+
 def migrate_paths(paths, root):
     return [str(Path(p).relative_to(Path(root))).strip('/') for p in paths]
+
 
 def migrate_split(ifile, ofile, root):
     with open(ifile, "rb") as f:
         filenames_str = pickle.load(f)
     unix_abs = all(fn.startswith('/') for fn in filenames_str)
-    if os.name == 'nt' and unix_abs: # Simon workaround
+    if os.name == 'nt' and unix_abs:  # Simon workaround
         # If the paths were already saved as Windows paths, as in the tests, do nothing
         # Explicitly not using type() and WindowsPath here, since this Class is not implemented on Linux
         # -> Check would not work
@@ -27,14 +29,16 @@ def migrate_split(ifile, ofile, root):
     with open(ofile, "wb") as f:
         pickle.dump(migrated, f)
 
+
 def main():
     parser = ArgumentParser(description="Migrates an absolute split pickle to relative paths")
     parser.add_argument("-i", "--input", help="The input file", required=True)
     parser.add_argument("-o", "--output", help="The output file", required=True)
-    parser.add_argument("-r", "--root", help="The data root, must be a prefix of every path stored in the input", required=True)
+    parser.add_argument("-r", "--root", help="The data root, must be a prefix of every loaded path", required=True)
 
     args = parser.parse_args()
     migrate_split(args.input, args.output, args.root)
+
 
 if __name__ == '__main__':
     main()
