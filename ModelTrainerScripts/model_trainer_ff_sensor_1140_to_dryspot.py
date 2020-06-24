8588b0cb46ffd875326fff6bba0ac8bd33a92c0b
Simon Stieber <stieber@isse.de>
2020-06-24 16:35:11 +0200

    Running script f. binary classification w. FF Sensor + Pretraining

--- ModelTrainerScripts/model_trainer_ff_sensor_1140_to_dryspot.py
+++ ModelTrainerScripts/model_trainer_ff_sensor_1140_to_dryspot.py
@@ -5,6 +5,7 @@ from torch.optim.lr_scheduler import ExponentialLR
 
 import Resources.training as r
 from Models.erfh5_ConvModel import SensorDeconvToDryspotEfficient2
+from Pipeline.data_loader_flowfront_sensor import DataloaderFlowfrontSensor
 from Pipeline.data_gather import get_filelist_within_folder_blacklisted
 from Pipeline.data_loader_dryspot import DataloaderDryspots
 from Trainer.ModelTrainer import ModelTrainer
@@ -14,7 +15,7 @@ from Utils.training_utils import read_cmd_params
 if __name__ == "__main__":
     args = read_cmd_params()
     checkpoint_path = r.chkp_FF_sensor_1140_to_ff
-    dlds = DataloaderDryspots()
+    dl = DataloaderFlowfrontSensor(image_size=(149, 117), frame_count=1, use_binary_sensor_only=True)
     m = ModelTrainer(
         lambda: SensorDeconvToDryspotEfficient2(
             pretrained="deconv_weights",
@@ -26,11 +27,11 @@ if __name__ == "__main__":
         cache_path=r.cache_path,
         batch_size=2048,
         train_print_frequency=100,
-        epochs=1000,
+        epochs=100,
         num_workers=75,
         num_validation_samples=131072,
         num_test_samples=1048576,
-        data_processing_function=dlds.get_sensor_bool_dryspot,
+        data_processing_function=dl.get_flowfront_sensor_bool_dryspot,
         data_gather_function=get_filelist_within_folder_blacklisted,
         loss_criterion=torch.nn.BCELoss(),
         optimizer_function=lambda params: torch.optim.AdamW(params, lr=1e-4),
