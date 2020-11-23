ff96b17da534a55fa6ebd61ddce11789dd9ce29a
Simon Stieber <simon.stieber@informatik.uni-augsburg.de>
2020-11-23 10:20:36 +0100

    Changed adding the whole source code of the data loader function to the caching hash to just adding a hash of the source code. Makes the output file more readable, should solve potential conflicts  in parsing the info json file. e.g. Special characters.

--- Utils/data_utils.py
+++ Utils/data_utils.py
@@ -101,7 +101,8 @@ def handle_torch_caching(processing_function, data_source_paths, sampler_func, b
     data_loader_info = processing_function.__self__.__dict__
     data_loader_info["data_processing_function"] = processing_function.__name__
     data_loader_info["data_loader_name"] = processing_function.__self__.__class__.__name__
-    data_loader_info["data_loader_source"] = inspect.getsource(processing_function.__self__.__class__)
+    source_str = inspect.getsource(processing_function.__self__.__class__)
+    data_loader_info["data_loader_source"] = hashlib.md5(source_str).hexdigest()
     data_loader_info["data_source_paths"] = [str(p) for p in data_source_paths]
     data_loader_info["batch_size"] = batch_size
     data_loader_info["num_validation_samples"] = num_val
