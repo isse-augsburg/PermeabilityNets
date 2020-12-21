3c45541c1703274f4444f5716da2b6c51b22f2cf
schroeni <niklas.schroeter@student.uni-augsburg.de>
2020-12-21 10:44:54 +0100

    updated paths to include new data

--- ModelTrainerScripts/flowfront_to_fiber.py
+++ ModelTrainerScripts/flowfront_to_fiber.py
@@ -10,7 +10,7 @@ from Trainer.ModelTrainer import ModelTrainer
 if __name__ == "__main__":
     args = read_cmd_params()
 
-    batch_size = 64
+    batch_size = 128
     dataset_paths = r.get_regular_sampled_data_paths()
     num_workers = 70
     num_val = 100
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         r.save_path,
         cache_path=r.cache_path,
         batch_size=batch_size,
-        epochs=10,
+        epochs=20,
         num_workers=num_workers,
         num_validation_samples=num_val,
         num_test_samples=num_test,
